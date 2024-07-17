69ebfbfbfa41dcc07b3b95df980967587c086875
dlacayoz <DANIEL.LACAYOZUNIGA@ucr.ac.cr>
2024-07-16 19:43:00 -0600

    Add files via upload

---
+++ src/main/java/Hub/ChallengeForo.java
@@ -0,0 +1,11 @@
+package api.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeForo {
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeForo.class, args);
+	}
+}
